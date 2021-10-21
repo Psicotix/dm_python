c211d4c3af7d40f94dcb90505e05f9cc326e2245
Psicotix <Psicotix@TheDebs>
2021-10-21 06:30:31 +0100

    Change name of punctuation() to punction()

---
+++ yesno.py
@@ -0,0 +1,24 @@
+def yesno(answer):
+    """
+    Checks positive or negative answer.
+    Will return "yes", "no" or "uncertain"
+    """
+    answer.lower()
+    yesno = ""
+    
+    answer_no = ["non", "no", "nah", "noo", "niet", "never", "no chance",
+                 "fuck off", "no fucking way", "fuck that", "no way", "not much"]
+    answer_yes = ["yes", "kinda", "kind of", "a bit", "slightly", "loads",
+                  "a little", "yeah", "yah", "yup", "yus", "si", "really", "ja", "of course"]
+    
+    if answer in answer_yes:
+        yesno = "yes"
+    elif answer in answer_no:
+        yesno = "no"
+    else:
+        yesno = "uncertain"
+    return yesno
+
+# test
+answer = input("Is it helping? ")
+print(yesno(answer))
