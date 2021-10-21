a2b32b3c6a7dd3902ec707d4703563b28eb10d2b
Psicotix <Psicotix@TheDebs>
2021-10-21 06:37:07 +0100

    File contains a function to check answers are either positive or negative. This particular commit to change the cockup I made with the last one, not defining this file.

--- yesno.py
+++ yesno.py
@@ -3,14 +3,14 @@ def yesno(answer):
     Checks positive or negative answer.
     Will return "yes", "no" or "uncertain"
     """
+    # Sort out inputs as answers.
     answer.lower()
     yesno = ""
-    
     answer_no = ["non", "no", "nah", "noo", "niet", "never", "no chance",
                  "fuck off", "no fucking way", "fuck that", "no way", "not much"]
     answer_yes = ["yes", "kinda", "kind of", "a bit", "slightly", "loads",
                   "a little", "yeah", "yah", "yup", "yus", "si", "really", "ja", "of course"]
-    
+
     if answer in answer_yes:
         yesno = "yes"
     elif answer in answer_no:
