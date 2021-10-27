de3b55650b7cab28ca08b141e641ca4227ea8df8
Psicotix <Psicotix@TheDebs>
2021-10-27 18:50:35 +0100

    Fixed an error that was discounting final item, switched negatives to positive in positive subsection.

--- yesno.py
+++ yesno.py
@@ -7,11 +7,9 @@ def yesno(answer):
                  "na", "nah", "not much good", "a bit shit", "a bit useless", "nope", "niet"]
     answer_yes = ["yes", "kinda", "kind of", "a bit", "a tiny bit", "a little bit", "a small", "slightly", "loads",
                   "a little", "yeah", "yah", "yup", "yus", "si", "really", "ja", "of course", "very much so",
-                  "yes siree", "yes sirree"]
+                  "yes siree", "yes sirree", "fuck yeah"]
     ans_status = ""
     an = len(answer_no)
-    if len(answer_no) >= 1:
-        an = an - 1
     n = 0
     # n should remain 0 or iteration until next item in list is checked.
     for x in answer_no:
@@ -34,18 +32,21 @@ def yesno(answer):
                 n += 1
                 # pass
             break
+    an = len(answer_yes)
+    # if len(answer_yes) >= 1:
+    #     an = an - 1
     n = 0
     for z in answer_yes:
         # while comes after for so that we iterate correctly, else for will always just check index[0]
         while n < an:
             # p is probably a string at this point already.
-            test_negative = z
-            test_negative = test_negative.split()
+            test_positive = z
+            test_positive = test_positive.split()
             test_answer = answer.split()
-            if all(x in test_negative for x in test_answer):
+            if all(z in test_positive for z in test_answer):
                 if True:
                     ans_status = "yes"
-                    n = len(answer_no)
+                    n = len(answer_yes)
                     if ans_status == "yes":
                         break
                     else:
@@ -55,7 +56,7 @@ def yesno(answer):
                 n += 1
                 # pass
             break
-        # Check uncertainty
+
         if not ans_status:
             # print("WTF?")
             print()
