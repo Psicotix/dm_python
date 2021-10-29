5e33d527b3538eab59222f5bf22a01bf67031efc
Psicotix <davesantorum@googlemail.com>
2021-10-29 15:58:08 +0100

    uncorrected working version - solution for json addressing included

--- spelling.py
+++ spelling.py
@@ -18,18 +18,50 @@ def spellcreator(name, description):
 
     return (spell_book)
     with open('spellbook.txt', 'a') as smells:
-        json.dumps(smells.write(spell_book))
+        smells.json.dumps(spell_book, indent = 4)
 
 # testing
 print("Make a new light spell!")
 name = "Light"
 description = "Casts a small amount of light around you, making it easier to see things"
+
+with open('spellbook.txt', 'a') as smells:
+    json.dump(spell_book, smells)
+
 spellcreator(name, description)
-print(spell_book)
+
+
 print("Make a fireball")
 name = "Fireball"
 description = "Throws a blob of molten something out of the palms of your hands like an old karate guy"
+
 spellcreator(name, description)
 
-with open('spellbook.txt', 'r') as smells:
-    print(smells.read())
+with open('spellbook.txt', 'a') as smells:
+    json.dump(spell_book, smells)
+    raise
+
+### THIS IS HOW TO READ AND ADD TO JSON FILE with structure as spellbook.json is looking.
+# code from https://www.freecodecamp.org/news/json-comment-example-how-to-comment-in-json-files/
+# import json
+ 
+ 
+# # function to add to JSON
+# def write_json(new_data, filename='data.json'):
+#     with open(filename,'r+') as file:
+#           # First we load existing data into a dict.
+#         file_data = json.load(file)
+#         # Join new_data with file_data inside emp_details
+#         file_data["emp_details"].append(new_data)
+#         # Sets file's current position at offset.
+#         file.seek(0)
+#         # convert back to json.
+#         json.dump(file_data, file, indent = 4)
+ 
+#     # python object to be appended
+# y = {"emp_name":"Nikhil",
+#      "email": "nikhil@geeksforgeeks.org",
+#      "job_profile": "Full Time"
+#     }
+     
+# write_json(y)
