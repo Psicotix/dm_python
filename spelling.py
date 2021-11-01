09b7436d1dbea1f8a5f42cd5e29abb59e2bf1ef1
Psicotix <davesantorum@googlemail.com>
2021-11-01 16:55:37 +0000

    Added check to see if JSON file is populated beyond the basic starting point.

--- spelling.py
+++ spelling.py
@@ -1,7 +1,7 @@
 #testing spell creation:
 import random
 import json
-import itertools
+import os
 
 spell_book = {}
 
@@ -34,25 +34,27 @@ def write_book(new_spell, filename='spellbook.json'):
         json.dump(spell_data, book, indent = 4)
 # code from https://www.geeksforgeeks.org/append-to-json-file-using-python/
 
-# testing 
+
+
 name = ""
 description = ""
+if os.path.getsize('spellbook.json') <= 25:
+    with open('spellbook.txt', 'r') as spell_list, open('spellhint.txt', 'r') as hints:
+        names_list = spell_list.readlines()
+        # so we now have a dirty list.
+        # print(names_list, type(names_list))
+        description_list = hints.readlines()
+        # we now have two lists. We want to take each element from the list.
+        # print(len(names_list)) # so we have our iterable total, currently 9
+        # so define a counter starting at 0
+        n = 0
+        while n < len(names_list):
+            name = names_list[n].strip()
+            description = description_list[n].strip()
+            # we have to clean up the name and description to prevent newline \n to be output in the JSON
 
-with open('spellbook.txt', 'r') as spell_list, open('spellhint.txt', 'r') as hints:
-
-    names_list = spell_list.readlines()
-    # so we now have a dirty list.
-    # print(names_list, type(names_list))
-    description_list = hints.readlines()
-    # we now have two lists. We want to take each element from the list.
-    # print(len(names_list)) # so we have our iterable total, currently 9
-    # so define a counter starting at 0
-    n = 0
-    while n < len(names_list):
-        name = names_list[n].strip()
-        description = description_list[n].strip()
-        # we have to clean up the name and description to prevent newline \n to be output in the JSON
-
-        spellcreator(name, description)  # create our unique spell
-        write_book(spell_book)  # write it _all_ to the JSON file
-        n += 1
+            spellcreator(name, description)  # create our unique spell
+            write_book(spell_book)  # write it _all_ to the JSON file
+            n += 1
+else:
+    print("Spell book already written! Don't bother doing it again!")
